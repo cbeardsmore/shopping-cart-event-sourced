227a3e86b4f44b3facbf33c1dfcceea365845943
Connor Beardsmore <connor.beardsmore@gmail.com>
2019-12-27 10:51:26 +0800

    REST tests for endpoints using fake spark server

--- domain/src/main/java/com/cbeardsmore/scart/domain/command/RemoveProductCommand.java
+++ domain/src/main/java/com/cbeardsmore/scart/domain/command/RemoveProductCommand.java
@@ -1,5 +1,6 @@
 package com.cbeardsmore.scart.domain.command;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public class RemoveProductCommand implements Command {
@@ -10,5 +11,24 @@ public class RemoveProductCommand implements Command {
     public RemoveProductCommand(UUID cartId, UUID productId) {
         this.cartId = cartId;
         this.productId = productId;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("RemoveProductCommand{cartId=%s,productId=%s}", cartId, productId);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RemoveProductCommand that = (RemoveProductCommand) o;
+        return Objects.equals(cartId, that.cartId) &&
+                Objects.equals(productId, that.productId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cartId, productId);
     }
 }
---
+++ rest/src/test/java/com/cbeardsmore/scart/rest/utils/CommandHandlerStub.java
@@ -0,0 +1,63 @@
+package com.cbeardsmore.scart.rest.utils;
+
+import com.cbeardsmore.scart.domain.CommandHandler;
+import com.cbeardsmore.scart.domain.command.AddProductCommand;
+import com.cbeardsmore.scart.domain.command.CheckoutCommand;
+import com.cbeardsmore.scart.domain.command.Command;
+import com.cbeardsmore.scart.domain.command.CreateCartCommand;
+import com.cbeardsmore.scart.domain.command.RemoveProductCommand;
+
+public class CommandHandlerStub extends CommandHandler {
+
+    private Command lastCommand;
+    private boolean throwNext;
+    private RuntimeException nextException;
+
+    CommandHandlerStub() {
+        super();
+    }
+
+    @Override
+    public Object handle(CreateCartCommand command) {
+        throwNextIfRequired();
+        lastCommand = command;
+        return null;
+    }
+
+    @Override
+    public Object handle(AddProductCommand command) {
+        throwNextIfRequired();
+        lastCommand = command;
+        return null;
+    }
+
+    @Override
+    public Object handle(RemoveProductCommand command) {
+        throwNextIfRequired();
+        lastCommand = command;
+        return null;
+    }
+
+    @Override
+    public Object handle(CheckoutCommand command) {
+        throwNextIfRequired();
+        lastCommand = command;
+        return null;
+    }
+
+    Command getLastCommand() {
+        return lastCommand;
+    }
+
+    void whenNextCommandThrow(RuntimeException e) {
+        this.throwNext = true;
+        this.nextException = e;
+    }
+
+    private void throwNextIfRequired() {
+        if (throwNext) {
+            throwNext = false;
+            throw nextException;
+        }
+    }
+}
